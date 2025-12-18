6a85cee52b89e16c9fa0c72fde71166b17ca089f
tiuliina <tiu.aarnio@gmail.com>
2025-12-18 19:43:44 +0200

    Add supabase key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const SUPABASE_URL = "https://zbnpdljtffddfymhxumx.supabase.co";
-export const SUPABASE_ANON_KEY = "PASTA_ANON_KEY_TÄHÄN";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibnBkbGp0ZmZkZGZ5bWh4dW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwMzIxNDAsImV4cCI6MjA4MTYwODE0MH0.4LThsV6kp_CI9m7zlxORFHPW5IANiPLtR1O_TkrXmdE";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
